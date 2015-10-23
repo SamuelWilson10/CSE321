d66aa61e74f513e26e7673f8a472e81a03e8815f
SamuelWilson10 <samuel_elo@hotmail.com>
2015-10-22 20:24:44 -0700

    Added Programming Exercises + Fixes

--- Assignments/Assignment 1/Programming Exercises/ex2Person.cpp
+++ Assignments/Assignment 1/Programming Exercises/ex2Person.cpp
@@ -8,9 +8,9 @@ Person::Person(const string & ln, const char * fn /* = "Heyyou" */) {
 };
 
 void Person::Show() const {
-  std::cout << *fname << " " << lname << std::endl; 
+  std::cout << fname << " " << lname << std::endl; 
 };
 
 void Person::FormalShow() const {
-  std::cout << lname << ", " << *fname << std::endl; 
+  std::cout << lname << ", " << fname << std::endl; 
 };
